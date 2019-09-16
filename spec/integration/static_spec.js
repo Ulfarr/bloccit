7a3ad24eebf64357736707e9b0c888df31529ddf
ulfarr <pipboy.moran9@gmail.com>
2019-09-16 13:46:23 -0500

    assignment 2 express

--- spec/integration/static_spec.js
+++ spec/integration/static_spec.js
@@ -1,6 +1,7 @@
 const request = require("request");
 const server = require("../../src/server");
 const base = "http://localhost:3000/";
+const marco = 'http://localhost:3000/marco';
 
 describe("routes : static", () => {
 
@@ -20,4 +21,18 @@ describe("routes : static", () => {
     });
 
   });
-});
+  describe("GET /marco", () => {
+    
+    it("should return status code 200 and have \'polo\' in the body", (done) => {
+    
+    request.get(marco, (err, res, body) => {
+
+        expect(res.statusCode).toBe(200);
+        expect(body).toBe("polo");
+        
+        done();
+      });
+     });   
+
+    });
+  });
