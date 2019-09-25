09932ca50e6e6b956810bd3be5cf4bb8de11e549
ulfarr <pipboy.moran9@gmail.com>
2019-09-25 11:33:27 -0500

    checkpoint topic resource

--- spec/integration/static_spec.js
+++ spec/integration/static_spec.js
@@ -2,23 +2,37 @@ const request = require("request");
 const server = require("../../src/server");
 const base = "http://localhost:3000/";
 
+
+
 describe("routes : static", () => {
 
-//#1
-  describe("GET /", () => {
-
-//#2
-it("should return status code 200 and have 'Welcome to Bloccit' in the body of the response", (done) => {
-
-//#3
-      request.get(base, (err, res, body) => {
-        expect(res.statusCode).toBe(200);
-        expect(body).toContain("Welcome to Bloccit");
-
-//#4
-        done();
+  //#1
+    describe("GET /", () => {
+  
+  //#2
+  it("should return status code 200 and have 'Welcome to Bloccit' in the body of the response", (done) => {
+  
+  //#3
+        request.get(base, (err, res, body) => {
+          expect(res.statusCode).toBe(200);
+          expect(body).toContain("Welcome to Bloccit");
+  
+  //#4
+          done();
+        });
       });
     });
-
+      describe("GET /about", () => {
+           
+  it("should return status code 200 and have About Us string in the body", (done) => {
+  
+    
+        request.get(base, (err, res, body) => {
+          expect(res.statusCode).toBe(200);
+          expect(body).toContain("About Us");
+          
+          done();
+        })
+      });
+    });
   });
-});
